0bca98b0f779aa903ac40deea2336980cbb02959
Zahra6653 <59842591+Zahra6653@users.noreply.github.com>
2020-05-29 12:25:53 +0530

    Add files via upload

---
+++ TrainingAssignments/Day4/LogExample.java
@@ -0,0 +1,17 @@
+package logFile;
+
+import org.apache.log4j.Logger;
+
+public class LogExample {
+	
+	static Logger logger=Logger.getLogger("LogExample.class");
+    public static void main(String args[]) 
+    {
+    	logger.debug("This is DEBUG message");
+    	logger.info("This is INFO message");
+    	logger.error("This is ERROR message");
+    	logger.fatal("This is FATAL message");
+	
+    }
+	
+}
